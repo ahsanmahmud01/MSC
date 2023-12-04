2ac72d645909e04c65d0bd05f82175d9effbf5ae
Ahsan <niloyk54bxn@gmail.com>
2023-12-05 00:46:39 +0600

    Restructure

--- index.js
+++ index.js
@@ -1,41 +1,46 @@
 var navbar = document.querySelector('.navbar');
-    var content = document.querySelector('.main-content');
-    var isMobile = window.innerWidth <= 600;
+var content = document.querySelector('.main-content');
+var isMobile = window.innerWidth <= 600;
 
-    // Hover toggle effect for PC
-    if (!isMobile) {
-        navbar.addEventListener('mouseenter', function() {
-            navbar.style.width = '200px';
-        });
-
-        navbar.addEventListener('mouseleave', function() {
-            if (!navbar.classList.contains('expanded')) {
-                navbar.style.width = '50px';
-            }
-        });
-    }
-
-    // On-click expansion for mobile
-    navbar.addEventListener('click', function() {
-        if (isMobile) {
-            navbar.classList.toggle('expanded');
-            content.style.marginLeft = navbar.classList.contains('expanded') ? '200px' : '0';
+if (!isMobile) {
+    navbar.addEventListener('mouseenter', function () {
+        if (!navbar.classList.contains('expanded')) {
+            navbar.style.transform = 'translateX(0)';
         }
     });
 
-    // Close sidebar on link click for mobile
-    var links = document.querySelectorAll('.navbar a');
-    links.forEach(function(link) {
-        link.addEventListener('click', function() {
-            if (isMobile) {
-                navbar.classList.remove('expanded');
-                content.style.marginLeft = '0';
-            }
-        });
+    navbar.addEventListener('mouseleave', function () {
+        if (!navbar.classList.contains('expanded')) {
+            navbar.style.transform = 'translateX(-130px)'; // Adjust this value based on your design
+        }
     });
+}
 
-    // Parallax effect on body
-    document.addEventListener('scroll', function() {
-        var scrolled = window.scrollY;
-        document.body.style.backgroundPositionY = -(scrolled * 0.3) + 'px';
+navbar.addEventListener('click', function () {
+    if (isMobile) {
+        navbar.classList.toggle('expanded');
+        content.style.marginLeft = navbar.classList.contains('expanded') ? '200px' : '70px';
+    }
+});
+
+var links = document.querySelectorAll('.navbar a');
+links.forEach(function (link) {
+    link.addEventListener('click', function () {
+        if (isMobile) {
+            navbar.classList.remove('expanded');
+            content.style.marginLeft = '70px';
+        }
     });
+});
+
+document.addEventListener('scroll', function () {
+    var scrolled = window.scrollY;
+    document.body.style.backgroundPositionY = -(scrolled * 0.3) + 'px';
+});
+
+// Adjust content margin if navbar is still expanded when the window is resized
+window.addEventListener('resize', function () {
+    if (isMobile && navbar.classList.contains('expanded')) {
+        content.style.marginLeft = '200px';
+    }
+});
